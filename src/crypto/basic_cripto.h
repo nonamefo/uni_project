c56230cd37b58c07349ce402cf52898b40798f61
Alex Korotkiy <mstshuher@gmail.com>
2025-05-17 04:12:47 +0300

    Update basic_cripto.h

--- src/crypto/basic_cripto.h
+++ src/crypto/basic_cripto.h
@@ -5,6 +5,9 @@ class basic_cripto{
     public:
     virtual std::string decode(const std::string message) = 0;
     virtual std::string encode(const std::string message) = 0;
+
+    virtual std::pair<std::string, std::string> encode_with_key(const std::string message) = 0;
+    virtual std::pair<std::string, std::string> decode_with_key(const std::string message, const std::string key) = 0;
 };
 
 
